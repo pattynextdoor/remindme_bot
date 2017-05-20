9111073dfd23da418325c20b0da9c658d66e03e9
Patrick Tumbucon <ptumb001@ucr.edu>
2017-05-20 12:59:02 -0700

    12:58

--- hello.py
+++ hello.py
@@ -15,11 +15,8 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 #^^ all this stuff is for authenticating
 
-twts = api.search(q="@wyd_bot")
 
-filename=open(argfile,'r') #opens text file
-f = filename.readlines() #string named 'f' that reads all the lines of the text file
-filename.close() #closes text file
+twts = api.search(q="@wyd_bot")
 
 #for line in f:
     #api.update_status(line) #updates status
@@ -28,19 +25,18 @@ filename.close() #closes text file
 #list of specific strings we want to check for in Tweets
 t = ['@wyd_bot', '@wyd_bot to wash the dishes']
 
-
-
 for s in twts:
     for i in t:
         if i == s.text:
             sn = s.user.screen_name #gets user's username
-            m = "@%s Expect a tweet at " % (sn) #creates message 'm', customized with user's username
+            sname = s.user.name
+            m = "@%(sn)s Okay %(sname)s, expect a tweet in an hour." % {'sn': sn, 'sname': sname}#creates message 'm', customized with user's username
             s = api.update_status(m, s.id) #updates status
-            time.sleep(3)
+            time.sleep(3) #arbitrary time
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+public_tweets = api.user_timeline(id=sn, count=1)
+for tweet in public_tweets: #prints all tweets that bot has
+    print tweet.favorite_count
 
 user = api.get_user(sn)
 
---
+++ keys.py
@@ -0,0 +1,6 @@
+keys = dict(
+	consumer_key =          'Lj2w4F4vzXZ653dSNkqzqmn10',
+	consumer_secret =       'WLGT1JUNJ3aNaHacS9deM9iYcj0w3NDJodVHKbaMpXbdACOHxA',
+	access_token =          '865969434170597376-dcKEGVfVlQnaZryFFglcrIE6RxNphba',
+	access_token_secret =   'KLFeCkjJVABDEywnM7z74cXsD401vlsmNm9pma9Aty11B'
+)
